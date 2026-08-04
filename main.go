6a3d4f81d7794a161a4f4a41a7bd07afb0714aa1
Ondrej Sika <ondrej@ondrejsika.com>
2026-08-04 16:48:30 +0200

    feat(pkg/bark): Move func Bark() to pkg/bark to be used in imports

--- main.go
+++ main.go
@@ -1,36 +1,7 @@
 package main
 
-import (
-	"bytes"
-	_ "embed"
-	"log"
-	"time"
-
-	"github.com/hajimehoshi/go-mp3"
-	"github.com/hajimehoshi/oto/v2"
-)
-
-//go:embed bark.mp3
-var barkMP3 []byte
+import "github.com/ondrejsikax/bark/pkg/bark"
 
 func main() {
-	decoder, err := mp3.NewDecoder(bytes.NewReader(barkMP3))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	ctx, ready, err := oto.NewContext(decoder.SampleRate(), 2, 2)
-	if err != nil {
-		log.Fatal(err)
-	}
-	<-ready
-
-	player := ctx.NewPlayer(decoder)
-	defer player.Close()
-
-	player.Play()
-
-	for player.IsPlaying() {
-		time.Sleep(100 * time.Millisecond)
-	}
+	bark.Bark()
 }
---
+++ pkg/bark/bark.go
@@ -0,0 +1,36 @@
+package bark
+
+import (
+	"bytes"
+	_ "embed"
+	"log"
+	"time"
+
+	"github.com/hajimehoshi/go-mp3"
+	"github.com/hajimehoshi/oto/v2"
+)
+
+//go:embed bark.mp3
+var barkMP3 []byte
+
+func Bark() {
+	decoder, err := mp3.NewDecoder(bytes.NewReader(barkMP3))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, ready, err := oto.NewContext(decoder.SampleRate(), 2, 2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	<-ready
+
+	player := ctx.NewPlayer(decoder)
+	defer player.Close()
+
+	player.Play()
+
+	for player.IsPlaying() {
+		time.Sleep(100 * time.Millisecond)
+	}
+}
