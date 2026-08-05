5989e3b66e8e01fcc245f3b4668397ea75aa5dcf
Ondrej Sika <ondrej@ondrejsika.com>
2026-08-05 15:25:56 +0200

    feat(pkg/bark): Play MP3 only on CGO_ENABLED=1 MacOS

--- pkg/bark/bark.go
+++ pkg/bark/bark.go
@@ -1,36 +1,9 @@
+//go:build !(darwin && cgo)
+
 package bark
 
-import (
-	"bytes"
-	_ "embed"
-	"log"
-	"time"
-
-	"github.com/hajimehoshi/go-mp3"
-	"github.com/hajimehoshi/oto/v2"
-)
-
-//go:embed bark.mp3
-var barkMP3 []byte
+import "log"
 
 func Bark() {
-	decoder, err := mp3.NewDecoder(bytes.NewReader(barkMP3))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	ctx, ready, err := oto.NewContext(decoder.SampleRate(), 2, 2)
-	if err != nil {
-		log.Fatal(err)
-	}
-	<-ready
-
-	player := ctx.NewPlayer(decoder)
-	defer player.Close()
-
-	player.Play()
-
-	for player.IsPlaying() {
-		time.Sleep(100 * time.Millisecond)
-	}
+	log.Println("bark: MP3 playback requires macOS with CGO_ENABLED=1")
 }
